38473e7dd68ac92309f4fb91aaaee1f8efae3d3b
FionaSelanno <fcs_fiona@hotmail.com>
2017-05-05 12:13:47 +0200

    Products chain: get data from database, make json file.

--- src/main/java/com/petsupplies/domain/Product.java
+++ src/main/java/com/petsupplies/domain/Product.java
@@ -1,7 +1,5 @@
 package com.petsupplies.domain;
 
-//import com.oracle.webservices.internal.api.EnvelopeStyle;
-//
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -24,12 +22,6 @@ public class Product {
     private String name;
     private String description;
     private double price;
-
-    public Product(String name, String description, double price) {
-        this.name = name;
-        this.description = description;
-        this.price = price;
-    }
 
     public String getName() {
         return name;
@@ -54,5 +46,6 @@ public class Product {
     public void setPrice(double price) {
         this.price = price;
     }
+
 }
 
